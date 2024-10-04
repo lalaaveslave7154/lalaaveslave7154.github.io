e325e3293a4ddd577b28d59a365947644106390b
lalaaveslave7154 <lalaaveslave@gmail.com>
2024-10-04 11:16:30 +0800

    Add files via upload

---
+++ hw2/script.js
@@ -0,0 +1,63 @@
+// 定義變數
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+let randomColor = Math.floor(Math.random()*16777215).toString(16);
+let x = 0, y = 0, dx = 10, dy = 10, r = 10, color = "#009500";
+
+// 畫圓形
+function drawBall() 
+{
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, Math.PI*2); // arc(圓心x, 圓心y, 半徑, 起始角, 結束角)
+	ctx.fillStyle = color;
+	ctx.fill();
+	ctx.closePath();
+}
+
+// 按下按鍵時觸發
+document.addEventListener("keydown", keyDownHandler);
+function keyDownHandler(e) 
+{
+	if(e.key == "ArrowRight")        x += dx;
+	else if(e.key == "ArrowLeft")    x -= dx;
+    else if(e.key == "ArrowUp")      y -= dy;
+	else if(e.key == "ArrowDown")    y += dy;
+}
+
+//看有沒有壓住
+document.addEventListener("mouseup", mouseupHandler);
+document.addEventListener("mousedown", mousedownHandler);
+function mousedownHandler (e)
+{
+	x = e.clientX - canvas.offsetLeft;
+	y = e.clientY - canvas.offsetTop;
+	color = "#"+ Math.floor(Math.random()*16777215).toString(16);
+	canMove = 1;
+	
+}
+function mouseupHandler ()
+{
+	canMove = 0;
+	
+}
+
+// TODO: 滑鼠移動(mousemove)時觸發，改變位置(x, y)為滑鼠目前位置(e.clientX, e.clientY)
+document.addEventListener("mousemove", mousemoveHandler);
+function mousemoveHandler(e)
+{
+	if(canMove)
+	{
+		x = e.clientX - canvas.offsetLeft;
+		y = e.clientY - canvas.offsetTop;
+	}
+}
+
+
+// 更新畫布
+function draw() 
+{	
+	//ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    requestAnimationFrame(draw);
+}
+draw();
